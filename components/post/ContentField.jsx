92d958cbf6e5cf96b9a4391ccd53389fe3211f09
Jerricke <eejerrickw@gmail.com>
2023-09-17 16:59:23 -0600

    added done feature for keyboard

--- components/post/ContentField.jsx
+++ components/post/ContentField.jsx
@@ -1,38 +1,52 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+    StyleSheet,
+    Keyboard,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 import React from "react";
 import { Input } from "react-native-elements";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, Feather } from "@expo/vector-icons";
 import { COLORS } from "../../constants/theme";
 
 function ContentField({ content, setContent }) {
-  return (
-    <View style={styles.container}>
-      <Input
-        rightIcon={
-          <TouchableOpacity
-            style={styles.reset}
-            onPress={() => setContent(null)}
-          >
-            <Ionicons name="remove" size={24} color="black" />
-          </TouchableOpacity>
-        }
-        multiline
-        placeholder="Content"
-        onChangeText={(text) => setContent(text)}
-        value={content}
-      />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Input
+                rightIcon={
+                    <View style={{ flexDirection: "row", gap: 10 }}>
+                        <TouchableOpacity
+                            style={styles.reset}
+                            onPress={() => setContent(null)}
+                        >
+                            <Ionicons name="remove" size={24} color="black" />
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.reset}
+                            onPress={() => Keyboard.dismiss()}
+                        >
+                            <Feather name="check" size={24} color="black" />
+                        </TouchableOpacity>
+                    </View>
+                }
+                multiline
+                placeholder="Content"
+                onChangeText={(text) => setContent(text)}
+                value={content}
+            />
+        </View>
+    );
 }
 
 export default ContentField;
 
 const styles = StyleSheet.create({
-  container: {
-    width: "90%",
-  },
-  reset: {
-    backgroundColor: COLORS.c3,
-    borderRadius: 4,
-  },
+    container: {
+        width: "90%",
+    },
+    reset: {
+        backgroundColor: COLORS.c3,
+        borderRadius: 4,
+    },
 });
--- components/post/TitleField.jsx
+++ components/post/TitleField.jsx
@@ -5,30 +5,34 @@ import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "../../constants/theme";
 
 function TitleField({ title, setTitle }) {
-  return (
-    <View style={styles.container}>
-      <Input
-        rightIcon={
-          <TouchableOpacity style={styles.reset} onPress={() => setTitle(null)}>
-            <Ionicons name="remove" size={24} color="black" />
-          </TouchableOpacity>
-        }
-        placeholder="Title"
-        onChangeText={(text) => setTitle(text)}
-        value={title}
-      />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Input
+                rightIcon={
+                    <TouchableOpacity
+                        style={styles.reset}
+                        onPress={() => setTitle(null)}
+                    >
+                        <Ionicons name="remove" size={24} color="black" />
+                    </TouchableOpacity>
+                }
+                placeholder="Title"
+                returnKeyType="done"
+                onChangeText={(text) => setTitle(text)}
+                value={title}
+            />
+        </View>
+    );
 }
 
 export default TitleField;
 
 const styles = StyleSheet.create({
-  container: {
-    width: "90%",
-  },
-  reset: {
-    backgroundColor: COLORS.c3,
-    borderRadius: 4,
-  },
+    container: {
+        width: "90%",
+    },
+    reset: {
+        backgroundColor: COLORS.c3,
+        borderRadius: 4,
+    },
 });
