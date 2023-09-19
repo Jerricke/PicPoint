31a417c3159c8e20aef871a53a2c1686affb2bac
Jerricke <eejerrickw@gmail.com>
2023-09-19 14:17:27 -0600

    implemented map view, still need to work on region change

--- app/(base)/(logged-in)/lp.js
+++ app/(base)/(logged-in)/lp.js
@@ -1,16 +1,81 @@
-import { View, Text } from "react-native";
+import { View, Text, KeyboardAvoidingView } from "react-native";
 import { Link, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { API_KEY } from "@env";
+import LocationPicker from "../../../components/post/LocationPicker";
 
 const lp = () => {
-    // If the page was reloaded or navigated to directly, then the modal should be presented as
-    // a full screen page. You may need to change the UI to account for this.
-    const isPresented = router.canGoBack();
+    const [isLoading, setIsLoading] = useState(true);
+    const [location, setLocation] = useState(null);
+    const [pin, setPin] = useState({});
+    const [region, setRegion] = useState({});
+
+    useEffect(() => {
+        const getPermissions = async () => {
+            const { status } =
+                await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                console.log("please grant location permissions");
+            }
+
+            const currentLocation = await Location.getCurrentPositionAsync({});
+            setLocation(currentLocation);
+            setPin({
+                longitude: currentLocation.coords.longitude,
+                latitude: currentLocation.coords.latitude,
+            });
+            setRegion({
+                longitude: currentLocation?.coords.longitude,
+                latitude: currentLocation?.coords.latitude,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.04,
+            });
+            setIsLoading(false);
+        };
+        getPermissions();
+    }, []);
+
     return (
-        <SafeAreaView>
-            <Text>test</Text>
-        </SafeAreaView>
+        <KeyboardAvoidingView behavior="padding" style={{ height: "100%" }}>
+            <LocationPicker
+                isLoading={isLoading}
+                location={location}
+                pin={pin}
+                setPin={setPin}
+            />
+            <View style={{ flex: 1 }}>
+                <GooglePlacesAutocomplete
+                    placeholder="Search"
+                    fetchDetails
+                    GooglePlacesSearchQuery={{
+                        rankby: "distance",
+                    }}
+                    onPress={(data, details = null) => {
+                        // 'details' is provided when fetchDetails = true
+                        console.log(details.geometry.location);
+                        setPin({
+                            longitude: details.geometry.location.lng,
+                            latitude: details.geometry.location.lat,
+                        });
+                        setRegion({
+                            longitude: details.geometry.location.lng,
+                            latitude: details.geometry.location.lat,
+                            latitudeDelta: 0.1,
+                            longitudeDelta: 0.04,
+                        });
+                    }}
+                    query={{
+                        key: API_KEY,
+                        language: "en",
+                        location: `${location?.longitude}, ${location?.latitude}`,
+                    }}
+                />
+            </View>
+        </KeyboardAvoidingView>
     );
 };
 
--- components/post/LocationPicker.jsx
+++ components/post/LocationPicker.jsx
@@ -1,10 +1,48 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { ActivityIndicator } from "react-native-paper";
 
-function LocationPicker() {
+function LocationPicker({ isLoading, location, pin, setPin, region }) {
+    useEffect(() => {
+        onRelocate();
+    }, [region]);
+
+    function onRelocate() {
+        this.mapRef?.animateToRegion(region);
+    }
+
     return (
-        <View>
-            <Text>LocationPicker</Text>
+        <View style={{ flex: 3 }}>
+            {isLoading ? (
+                <ActivityIndicator style={{ flex: 1 }} size={52} color="red" />
+            ) : (
+                <MapView
+                    provider="google"
+                    style={{ flex: 1 }}
+                    ref={(ref) => {
+                        this.mapRef = ref;
+                    }}
+                    initialRegion={{
+                        latitude: location?.coords.latitude,
+                        longitude: location?.coords.longitude,
+                        latitudeDelta: 0.1,
+                        longitudeDelta: 0.04,
+                    }}
+                >
+                    <Marker
+                        coordinate={pin}
+                        pinColor="red"
+                        draggable
+                        onDragEnd={(e) => {
+                            setPin({
+                                latitude: e.nativeEvent.coordinate.latitude,
+                                longitude: e.nativeEvent.coordinate.longitude,
+                            });
+                        }}
+                    />
+                </MapView>
+            )}
         </View>
     );
 }
